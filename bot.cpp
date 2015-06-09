ca0f66dfc70e6819dc1a76080fddc114a6b0561e
MateuszJanczura <mateusz.janczura@gmail.com>
2015-06-10 01:06:32 +0200

    Konstruktory klasy bot

--- bot.cpp
+++ bot.cpp
@@ -6,23 +6,49 @@
 
 class bot
 {
+	private:
+		void inicjalizacja();
+		
+		
 	public:
 		double sA, sB, sC; //parametry bota
 		int sKonta, sPrzedmioty, nazwa;
+		
 		bot();
+		bot(bot* a); //konstruktor kopiujacy
+		bot(bot* a, bot* b); //konstruktor krzyzowania
 		bot(double x, double y, double z, int konto);
+		
 		bool wynik(int cena, int wartoscPrzedmiotu);
 		bool operator< (bot const &q) const {return sPrzedmioty > q.sPrzedmioty;} // bot jest mniejszy od innego jesli posiada gorsze przedmioty
 };
 
+void bot::inicjalizacja()
+{
+	sKonta = sA = sB = sC = sPrzedmioty = 0;
+}
+
 bot::bot()
 {
-	sKonta = sPrzedmioty = 0;
+	inicjalizacja();
+}
+
+bot::bot(bot* a)
+{
+	inicjalizacja();
+	sA = a -> sA;
+	sB = a -> sB;
+	sC = a -> sC;
+}
+
+bot::bot(bot* a, bot* b)
+{
+	inicjalizacja();
 }
 
 bot::bot(double x, double y, double z, int konto)
 {
-	sKonta = sPrzedmioty = 0;
+	inicjalizacja();
 	sA = x;
 	sB = y;
 	sC = z;
